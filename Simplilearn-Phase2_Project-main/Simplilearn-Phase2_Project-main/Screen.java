f6e592f4b389040518faea9a9310187ec39c129b
Spoorthi88 <119590571+Spoorthi88@users.noreply.github.com>
2022-12-03 12:22:47 +0530

    Add files via upload

---
+++ Simplilearn-Phase2_Project-main/Simplilearn-Phase2_Project-main/Screen.java
@@ -0,0 +1,10 @@
+package com.lockedme.lockers.diaplaymenu;
+
+public interface Screen {
+    public void show();
+
+    public void navigateOption(int option);
+    
+    public void getUserInput();
+
+}
